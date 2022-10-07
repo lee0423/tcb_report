be6199716ec6cd91ced9a846e672d3deadf01dfb
Lee Liao <leelovemay@gmail.com>
2022-10-08 00:26:21 +0800

    seperate code

--- new_pdf.py
+++ new_pdf.py
@@ -76,44 +76,14 @@ def create_report(pdf_name):
         if path.endswith(".xlsx"):
             count += 1 
 
-    for i in range(1, count+1):
-
-        if i % 2 != 0:
-            df = pd.read_excel(f'/Users/liaoyi-ming/Desktop/tcb_report/{i}.xlsx')
-            plt.figure()
-            chart_title = df.iloc[0,1]
-            # df['EventTime']=pd.to_datetime(df['EventTime'])
-            df = df.pivot(index='EventTime', columns='EntityName', values='Value')
-            df.plot( figsize=(15, 7), title=chart_title, grid=True)
-            # df.plot(x='EventTime', y='Value',figsize=(11,5), title=chart_title, kind='line')
-            img_buf = io.BytesIO()  # Create image object
-            plt.savefig(img_buf, dpi=200)  # Save the image
-            pdf.add_page()
-            pdf.image(img_buf,x=-1.7, w=pdf.epw+5, y=3)
-            # pdf.set_auto_page_break(auto = True, margin = 10)
-        else:
-            df = pd.read_excel(f'/Users/liaoyi-ming/Desktop/tcb_report/{i}.xlsx')
-            plt.figure()
-            chart_title = df.iloc[0,1]
-            # df['EventTime']=pd.to_datetime(df['EventTime'])
-            df = df.pivot(index='EventTime', columns='EntityName', values='Value')
-            df.plot( figsize=(15, 7), title=chart_title, grid=True)
-            # df.plot(x='EventTime', y='Value',figsize=(11,5), title=chart_title, kind='line')
-            img_buf = io.BytesIO()  # Create image object
-            plt.savefig(img_buf, dpi=200)  # Save the image
-            # pdf.add_page()
-            pdf.image(img_buf,x=-1.7, w=pdf.epw+5, y=15)
-            pdf.set_auto_page_break(auto = True, margin = 10)
-        # pdf.cell(None,None, txt="Back to Top", markdown=True,)
+    #Start adding chart to PDF file.
+    report_images(pdf=pdf, count=count)
 
     # get total page numbers
     pdf.alias_nb_pages()
 
     # Set auto page break
     pdf.set_auto_page_break(auto = True, margin = 10)
-    # pdf.add_page()
-    # pdf.set_text_color(r=169,g=169,b=169)
-    # pdf.report_content()
     pdf.output(pdf_name)
 
 if __name__ == "__main__":
